26605437eab44b18f3e716f89ab0b366b863394d
Jan Fousek <izaak@mail.muni.cz>
2015-12-06 17:12:29 +0100

    Fixed coupling time offset from scheme (lowers delay, not increases).

--- src/coupling.cc
+++ src/coupling.cc
@@ -13,7 +13,7 @@ void linear_coupling::operator()(	const local_connectivity_type &connectivity,
 	for(	local_connectivity_type::const_iterator connection = connectivity.begin();
 			connection != connectivity.end(); connection++){
 		int from = connection->from;
-		double delay = connection->delay + time_offset;
+		double delay = connection->delay - time_offset;
 
 		local_state_type remote_state = global_history[from]->get_values(delay);
 		for(local_state_type::size_type dim=0; dim < coupling.size(); dim++){
