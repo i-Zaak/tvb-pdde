11021fe58577be143820e945d902fb63f3b7f88e
Jan Fousek <izaak@mail.muni.cz>
2015-12-09 18:03:14 +0100

    allow output both for metis and simulator.

--- data/mtx_to_adj_list.py
+++ data/mtx_to_adj_list.py
@@ -4,10 +4,14 @@ import numpy as np
 import sys
 
 if __name__ == "__main__":
-    if len(sys.argv) != 3:
-        print "Usage: %s matrix_in.mtx matrix_out.adj" % sys.argv[0]
+    if len(sys.argv) != 4:
+        print "Usage: %s matrix_in.mtx matrix_out.adj [float|int]" % sys.argv[0]
         sys.exit(1)
-
+    # the weight could be also length of the buffer of particular connection
+    if sys.argv[3] == 'int':
+        metis_format = True
+    else:
+        metis_format = False
     print "Reading... ",
     sys.stdout.flush()
     A = mmread(sys.argv[1])    
@@ -27,8 +31,10 @@ if __name__ == "__main__":
             #line = "%d "%(i+1) 
             line = ""
             for j in xrange(A.indptr[i], A.indptr[i+1]):
-                #line+= " %d %f" %(A.indices[j]+1,A.data[j])
-                line+= " %d %d" %(A.indices[j]+1,int(1/(A.data[j])*1000)) #here the weight could be length of the buffer of particular connection
+                if metis_format:
+                    line+= " %d %d" %(A.indices[j]+1,int(1/(A.data[j])*1000)) 
+                else:
+                    line+= " %d %f" %(A.indices[j]+1,A.data[j])
             line += "\n"
             out_f.write(line)
     print "Done!"
